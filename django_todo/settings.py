f0a27299ae9464138ddbea71dbad418d61518777
andrebraga7 <andrestrevisan@gmail.com>
2023-01-13 12:47:46 +0000

    Add missing SECRET_KEY and DEBUG variables to the settings.py

--- django_todo/settings.py
+++ django_todo/settings.py
@@ -17,6 +17,10 @@ if os.path.exists("env.py"):
     import env
 
 
+SECRET_KEY = os.environ.get("SECRET_KEY")
+
+DEBUG = os.environ.get("DEBUG")
+
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
