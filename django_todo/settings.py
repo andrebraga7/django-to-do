67d19e4866de40dac75e2d46273939f49df10605
andrebraga7 <andrestrevisan@gmail.com>
2023-01-14 12:41:01 +0000

    Fixed allowed hosts

--- django_todo/settings.py
+++ django_todo/settings.py
@@ -29,7 +29,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['todo-app-andrebraga7.herokuapp.com']
 
 
 # Application definition
